1f54d638603564b05c994e5d23a0ce30381b6b5f
Kirill Pavlovskii <kirill-pavlovskij@rambler.ru>
2023-03-13 09:46:52 +0300

    first commit

---
+++ src/myPick.ts
@@ -0,0 +1,16 @@
+type MyPick<T, K extends keyof T> = {
+  [Key in K]: T[K];
+}
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+type TodoPreview = MyPick<Todo, 'title' | 'completed'>
+
+const todo: TodoPreview = {
+  title: 'Clean room',
+  completed: false,
+}
